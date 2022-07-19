18ae1d7ebea913970f58a6c6fd5f0b3f3c93363a
Cybug Timehin <93569544+Cybugg@users.noreply.github.com>
2022-07-19 22:46:42 +0100

    Update keyboard.jsx

--- wordle/src/components/keyboard.jsx
+++ wordle/src/components/keyboard.jsx
@@ -1,19 +1,52 @@
-import { BackspaceOutlined } from '@mui/icons-material';
 import React from 'react'
+import { useContext } from 'react';
+import { useCallback } from 'react';
 import { useEffect } from 'react';
+import { AppContext } from './App';
 import Key from './Key';
 
 export default function Keyboard() {
+
+    const {onSelectLetter,onDelete,onEnter} = useContext(AppContext)
+
      const keys1 = ["Q","W","E","R","T","Y","U","I","O","P"];
      const keys2 =["A","S","D","F","G","H","j","K","L"];
      const keys3 =["Z","X","C","V","B","N","M"]; 
-
+const handlekeyboard = useCallback(
+  (e)=>{
+    if (e.key ==="Enter"){
+       onEnter()
+    }
+    else if(e.key === "Backspace"){
+       onDelete()
+    }
+    else{
+      keys1.map(
+        ele =>{ if(e.key === ele){
+      onSelectLetter()
+        }})
+      keys2.map(
+        ele =>{ if(e.key === ele){
+      onSelectLetter()
+        }})
+      keys3.map(
+        ele =>{ if(e.key === ele){
+      onSelectLetter()
+        }})
+    }
+  }
+)
   useEffect(
-    ()
+    ()=>{
+      document.addEventListener("keydown",handlekeyboard);
+      return ()=>{
+        document.removeEventListener("keydown",handlekeyboard);
+      }
+    },[handlekeyboard]
   )
 
   return (
-    <div className='keyboard mx-auto text-white'>
+    <div className='keyboard mx-auto text-white' onKeyDown={handlekeyboard}>
       <div className="line1 d-flex">{keys1.map(
         key => <Key keyVal={key} />
       )}</div>
